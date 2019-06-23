78b40e20b6d17e9a317dd55ce0647dfd6e462259
Daniel Buckley <buckley.daniel.n@gmail.com>
2019-06-22 20:27:20 -0600

    Add all properties and reorganize library code

--- TEA5767N.cpp
+++ TEA5767N.cpp
@@ -18,363 +18,486 @@
 #include <Wire.h>
 #include <TEA5767N.h>
 
-TEA5767N::TEA5767N() {
-  Wire.begin();
-  initializeTransmissionData();
-  muted = false;
+TEA5767N::TEA5767N()
+{
+	Wire.begin();
+	initializeTransmissionData();
+
+	muted = false;
+	mutedLeft = false;
+	mutedRight = false;
+	softMuted = false;
+	highCutControl = false;
+	stereoNoiseCancelling = false;
+	standby = false;
+	forcedMono = false;
 }
 
-void TEA5767N::initializeTransmissionData() {
-  transmission_data[FIRST_DATA] = 0;            //MUTE: 0 - not muted
-                                                //SEARCH MODE: 0 - not in search mode
-	
-  transmission_data[SECOND_DATA] = 0;           //No frequency defined yet
-	
-  transmission_data[THIRD_DATA] = 0xB0;         //10110000
-                                                //SUD: 1 - search up
-                                                //SSL[1:0]: 01 - low; level ADC output = 5
-                                                //HLSI: 1 - high side LO injection
-                                                //MS: 0 - stereo ON
-                                                //MR: 0 - right audio channel is not muted
-                                                //ML: 0 - left audio channel is not muted
-                                                //SWP1: 0 - port 1 is LOW
-	
-  transmission_data[FOURTH_DATA] = 0x10;        //00010000
-                                                //SWP2: 0 - port 2 is LOW
-                                                //STBY: 0 - not in Standby mode
-                                                //BL: 0 - US/Europe FM band
-                                                //XTAL: 1 - 32.768 kHz
-                                                //SMUTE: 0 - soft mute is OFF
-                                                //HCC: 0 - high cut control is OFF
-                                                //SNC: 0 - stereo noise cancelling is OFF
-                                                //SI: 0 - pin SWPORT1 is software programmable port 1
-	
-  transmission_data[FIFTH_DATA] = 0x00;         //PLLREF: 0 - the 6.5 MHz reference frequency for the PLL is disabled
-                                                //DTC: 0 - the de-emphasis time constant is 50 ms
+void TEA5767N::initializeTransmissionData()
+{
+	transmission_data[FIRST_DATA] = 0; //MUTE: 0 - not muted
+																		 //SEARCH MODE: 0 - not in search mode
+
+	transmission_data[SECOND_DATA] = 0; //No frequency defined yet
+
+	transmission_data[THIRD_DATA] = 0xB0; //10110000
+																				//SUD: 1 - search up
+																				//SSL[1:0]: 01 - low; level ADC output = 5
+																				//HLSI: 1 - high side LO injection
+																				//MS: 0 - stereo ON
+																				//MR: 0 - right audio channel is not muted
+																				//ML: 0 - left audio channel is not muted
+																				//SWP1: 0 - port 1 is LOW
+
+	transmission_data[FOURTH_DATA] = 0x10; //00010000
+																				 //SWP2: 0 - port 2 is LOW
+																				 //STBY: 0 - not in Standby mode
+																				 //BL: 0 - US/Europe FM band
+																				 //XTAL: 1 - 32.768 kHz
+																				 //SMUTE: 0 - soft mute is OFF
+																				 //HCC: 0 - high cut control is OFF
+																				 //SNC: 0 - stereo noise cancelling is OFF
+																				 //SI: 0 - pin SWPORT1 is software programmable port 1
+
+	transmission_data[FIFTH_DATA] = 0x00; //PLLREF: 0 - the 6.5 MHz reference frequency for the PLL is disabled
+																				//DTC: 0 - the de-emphasis time constant is 50 ms
 }
 
-void TEA5767N::calculateOptimalHiLoInjection(float freq) {
-	byte signalHigh;
-	byte signalLow;
-	
-	setHighSideLOInjection();
-	transmitFrequency((float) (freq + 0.45));
-	
-	signalHigh = getSignalLevel();
-	
-	setLowSideLOInjection();
-	transmitFrequency((float) (freq - 0.45));
-	
-	signalLow = getSignalLevel();
-
-	hiInjection = (signalHigh < signalLow) ? 1 : 0;
-}
-
-void TEA5767N::setFrequency(float _frequency) {
-	frequency = _frequency;
-	unsigned int frequencyW;
-	
-	if (hiInjection) {
-		setHighSideLOInjection();
-		frequencyW = 4 * ((frequency * 1000000) + 225000) / 32768;
-	} else {
-		setLowSideLOInjection();
-		frequencyW = 4 * ((frequency * 1000000) - 225000) / 32768;
-	}
-	
-	transmission_data[FIRST_DATA] = ((transmission_data[FIRST_DATA] & 0xC0) | ((frequencyW >> 8) & 0x3F));
-	transmission_data[SECOND_DATA] = frequencyW & 0XFF;
-}
-
-void TEA5767N::transmitData() {
+void TEA5767N::transmitData()
+{
 	Wire.beginTransmission(TEA5767_I2C_ADDRESS);
-	for (int i=0 ; i<6 ; i++) {
+	for (int i = 0; i < 5; i++)
+	{
 		Wire.write(transmission_data[i]);
 	}
 	Wire.endTransmission();
 	delay(100);
 }
 
-void TEA5767N::mute() {
-	muted = true;
-	setSoundOff();
-	transmitData();
-}
-
-void TEA5767N::setSoundOff() {
-	transmission_data[FIRST_DATA] |= 0b10000000;
-}
-
-void TEA5767N::turnTheSoundBackOn() {
-	muted = false;
-	setSoundOn();
-	transmitData();
-}
-
-void TEA5767N::setSoundOn() {
-	transmission_data[FIRST_DATA] &= 0b01111111;
-}
-
-boolean TEA5767N::isMuted() {
-	return muted;
-}
-
-void TEA5767N::transmitFrequency(float frequency) {
-	setFrequency(frequency);
-	transmitData();
-}
-
-void TEA5767N::selectFrequency(float frequency) {
-	calculateOptimalHiLoInjection(frequency);
-	transmitFrequency(frequency);
-}
-
-void TEA5767N::selectFrequencyMuting(float frequency) {
-	mute();
-	calculateOptimalHiLoInjection(frequency);
-	transmitFrequency(frequency);
-	turnTheSoundBackOn();
-}
-
-void TEA5767N::readStatus() {
-	Wire.requestFrom (TEA5767_I2C_ADDRESS, 5);
-	
-	if (Wire.available ()) {
-		for (int i = 0; i < 5; i++) {
+void TEA5767N::readStatus()
+{
+	Wire.requestFrom(TEA5767_I2C_ADDRESS, 5);
+	if (Wire.available())
+	{
+		for (int i = 0; i < 5; i++)
+		{
 			reception_data[i] = Wire.read();
 		}
 	}
 	delay(100);
 }
 
-float TEA5767N::readFrequencyInMHz() {
-	loadFrequency();
-	
-	unsigned int frequencyW = (((reception_data[FIRST_DATA] & 0x3F) * 256) + reception_data[SECOND_DATA]);
-	return getFrequencyInMHz(frequencyW);
+void TEA5767N::calculateOptimalLOInjection(float freq)
+{
+	byte signalHigh;
+	byte signalLow;
+
+	setHighSideLOInjection();
+	setFrequency((float)(freq + 0.45));
+	signalHigh = getSignalLevel();
+
+	setLowSideLOInjection();
+	setFrequency((float)(freq - 0.45));
+	signalLow = getSignalLevel();
+
+	hiInjection = (signalHigh < signalLow) ? 1 : 0;
 }
 
-void TEA5767N::loadFrequency() {
+void TEA5767N::setFrequency(float _frequency)
+{
+	frequency = _frequency;
+	unsigned int frequencyW;
+
+	if (hiInjection)
+	{
+		setHighSideLOInjection();
+		frequencyW = 4 * ((frequency * 1000000) + 225000) / 32768;
+	}
+	else
+	{
+		setLowSideLOInjection();
+		frequencyW = 4 * ((frequency * 1000000) - 225000) / 32768;
+	}
+
+	transmission_data[FIRST_DATA] = ((transmission_data[FIRST_DATA] & 0xC0) | ((frequencyW >> 8) & 0x3F));
+	transmission_data[SECOND_DATA] = frequencyW & 0XFF;
+}
+
+void TEA5767N::toggleMute()
+{
+	muted = !muted;
+
+	if (muted)
+	{
+		transmission_data[FIRST_DATA] |= 0b10000000;
+	}
+	else
+	{
+		transmission_data[FIRST_DATA] &= 0b01111111;
+	}
+
+	transmitData();
+}
+
+void TEA5767N::toggleMuteLeft()
+{
+	mutedLeft = !mutedLeft;
+
+	if (mutedRight)
+	{
+		transmission_data[THIRD_DATA] |= 0b00000010;
+	}
+	else
+	{
+		transmission_data[THIRD_DATA] &= 0b11111101;
+	}
+
+	transmitData();
+}
+
+void TEA5767N::toggleMuteRight()
+{
+	mutedRight = !mutedRight;
+
+	if (mutedRight)
+	{
+		transmission_data[THIRD_DATA] |= 0b00000100;
+	}
+	else
+	{
+		transmission_data[THIRD_DATA] &= 0b11111011;
+	}
+
+	transmitData();
+}
+
+void TEA5767N::toggleSoftMute()
+{
+	softMuted = !softMuted;
+
+	if (softMuted)
+	{
+		transmission_data[FOURTH_DATA] |= 0b00001000;
+	}
+	else
+	{
+		transmission_data[FOURTH_DATA] &= 0b11110111;
+	}
+
+	transmitData();
+}
+
+boolean TEA5767N::isMuted()
+{
+	return muted;
+}
+
+boolean TEA5767N::isMutedLeft()
+{
+	return mutedLeft;
+}
+
+boolean TEA5767N::isMutedRight()
+{
+	return mutedRight;
+}
+
+boolean TEA5767N::isSoftMuted()
+{
+	return softMuted;
+}
+
+void TEA5767N::selectFrequency(float frequency, boolean muteDuringUpdate)
+{
+	if (muteDuringUpdate)
+	{
+		muted = false;
+		toggleMute();
+	}
+
+	calculateOptimalLOInjection(frequency);
+	setFrequency(frequency);
+
+	if (muteDuringUpdate)
+	{
+		toggleMute();
+	}
+}
+
+void TEA5767N::selectChannel(int channel, boolean muteDuringUpdate)
+{
+	if (muteDuringUpdate)
+	{
+		muted = false;
+		toggleMute();
+	}
+
+	float frequency = 87.9 + ((channel - 200) * 0.2);
+	selectFrequency(frequency, false);
+
+	if (muteDuringUpdate)
+	{
+		toggleMute();
+	}
+}
+
+void TEA5767N::loadFrequency()
+{
 	readStatus();
-	
-	//Stores the read frequency that can be the result of a search and it´s not yet in transmission data
-	//and is necessary to subsequent calls to search.
+
 	transmission_data[FIRST_DATA] = (transmission_data[FIRST_DATA] & 0xC0) | (reception_data[FIRST_DATA] & 0x3F);
 	transmission_data[SECOND_DATA] = reception_data[SECOND_DATA];
 }
 
-float TEA5767N::getFrequencyInMHz(unsigned int frequencyW) {
-	if (hiInjection) {
+float TEA5767N::getFrequencyInMHz(unsigned int frequencyW)
+{
+	if (hiInjection)
+	{
 		return (((frequencyW / 4.0) * 32768.0) - 225000.0) / 1000000.0;
-	} else {
+	}
+	else
+	{
 		return (((frequencyW / 4.0) * 32768.0) + 225000.0) / 1000000.0;
 	}
 }
 
-void TEA5767N::setSearchUp() {
+float TEA5767N::readFrequencyInMHz()
+{
+	loadFrequency();
+
+	unsigned int frequencyW = (((reception_data[FIRST_DATA] & 0x3F) * 256) + reception_data[SECOND_DATA]);
+	return getFrequencyInMHz(frequencyW);
+}
+
+void TEA5767N::setSearchUp()
+{
 	transmission_data[THIRD_DATA] |= 0b10000000;
 }
 
-void TEA5767N::setSearchDown() {
+void TEA5767N::setSearchDown()
+{
 	transmission_data[THIRD_DATA] &= 0b01111111;
 }
 
-void TEA5767N::setSearchLowStopLevel() {
+void TEA5767N::setSearchLowStopLevel()
+{
 	transmission_data[THIRD_DATA] &= 0b10011111;
 	transmission_data[THIRD_DATA] |= (LOW_STOP_LEVEL << 5);
 }
 
-void TEA5767N::setSearchMidStopLevel() {
+void TEA5767N::setSearchMidStopLevel()
+{
 	transmission_data[THIRD_DATA] &= 0b10011111;
 	transmission_data[THIRD_DATA] |= (MID_STOP_LEVEL << 5);
 }
 
-void TEA5767N::setSearchHighStopLevel() {
+void TEA5767N::setSearchHighStopLevel()
+{
 	transmission_data[THIRD_DATA] &= 0b10011111;
 	transmission_data[THIRD_DATA] |= (HIGH_STOP_LEVEL << 5);
 }
 
-void TEA5767N::setHighSideLOInjection() {
+void TEA5767N::setHighSideLOInjection()
+{
 	transmission_data[THIRD_DATA] |= 0b00010000;
 }
 
-void TEA5767N::setLowSideLOInjection() {
+void TEA5767N::setLowSideLOInjection()
+{
 	transmission_data[THIRD_DATA] &= 0b11101111;
 }
 
-byte TEA5767N::searchNextMuting() {
+byte TEA5767N::searchNext(boolean muteDuringSearch)
+{
 	byte bandLimitReached;
-	
-	mute();
-	bandLimitReached = searchNext();
-	turnTheSoundBackOn();
-	
-	return bandLimitReached;
-}
 
-byte TEA5767N::searchNext() {
-	byte bandLimitReached;
-	
-	if (isSearchUp()) {
-		selectFrequency(readFrequencyInMHz() + 0.1);
-	} else {
-		selectFrequency(readFrequencyInMHz() - 0.1);
+	if (muteDuringSearch)
+	{
+		muted = false;
+		toggleMute();
 	}
-	
-	//Turns the search on
+
+	if (isSearchUp())
+	{
+		selectFrequency(readFrequencyInMHz() + 0.1, false);
+	}
+	else
+	{
+		selectFrequency(readFrequencyInMHz() - 0.1, false);
+	}
+
 	transmission_data[FIRST_DATA] |= 0b01000000;
 	transmitData();
-		
-	while(!isReady()) { }
-	//Read Band Limit flag
+
+	while (!isReady())
+	{
+	}
+
 	bandLimitReached = isBandLimitReached();
-	//Loads the new selected frequency
 	loadFrequency();
-	
-	//Turns de search off
+
 	transmission_data[FIRST_DATA] &= 0b10111111;
 	transmitData();
-	
+
+	if (muteDuringSearch)
+	{
+		toggleMute();
+	}
+
 	return bandLimitReached;
 }
 
-byte TEA5767N::startsSearchMutingFromBeginning() {
+byte TEA5767N::searchFrom(float frequency, boolean muteDuringSearch)
+{
+	selectFrequency(frequency, false);
+	return searchNext(muteDuringSearch);
+}
+
+byte TEA5767N::searchFromBeginning(boolean muteDuringSearch)
+{
 	byte bandLimitReached;
-	
-	mute();
-	bandLimitReached = startsSearchFromBeginning();
-	turnTheSoundBackOn();
-	
-	return bandLimitReached;
-}
-
-byte TEA5767N::startsSearchMutingFromEnd() {
-	byte bandLimitReached;
-	
-	mute();
-	bandLimitReached = startsSearchFromEnd();
-	turnTheSoundBackOn();
-	
-	return bandLimitReached;
-}
-
-byte TEA5767N::startsSearchFromBeginning() {
 	setSearchUp();
-	return startsSearchFrom(87.0);
+
+	if (muteDuringSearch)
+	{
+		muted = false;
+		toggleMute();
+	}
+
+	bandLimitReached = searchFrom(87.0, false);
+
+	if (muteDuringSearch)
+	{
+		toggleMute();
+	}
+
+	return bandLimitReached;
 }
 
-byte TEA5767N::startsSearchFromEnd() {
+byte TEA5767N::searchFromEnd(boolean muteDuringSearch)
+{
+	byte bandLimitReached;
 	setSearchDown();
-	return startsSearchFrom(108.0);
+
+	if (muteDuringSearch)
+	{
+		muted = false;
+		toggleMute();
+	}
+
+	bandLimitReached = searchFrom(108.0, false);
+
+	if (muteDuringSearch)
+	{
+		toggleMute();
+	}
+
+	return bandLimitReached;
 }
 
-byte TEA5767N::startsSearchFrom(float frequency) {
-	selectFrequency(frequency);
-	return searchNext();
-}
-
-byte TEA5767N::getSignalLevel() {
-	//Necessary before read status
+byte TEA5767N::getSignalLevel()
+{
+	// Necessary before read status
 	transmitData();
-	//Read updated status
+	// Read updated status
 	readStatus();
 	return reception_data[FOURTH_DATA] >> 4;
 }
 
-byte TEA5767N::isStereo() {
+byte TEA5767N::isStereo()
+{
 	readStatus();
 	return reception_data[THIRD_DATA] >> 7;
 }
 
-byte TEA5767N::isReady() {
+byte TEA5767N::isReady()
+{
 	readStatus();
 	return reception_data[FIRST_DATA] >> 7;
 }
 
-byte TEA5767N::isBandLimitReached() {
+byte TEA5767N::isBandLimitReached()
+{
 	readStatus();
 	return (reception_data[FIRST_DATA] >> 6) & 1;
 }
 
-byte TEA5767N::isSearchUp() {
+byte TEA5767N::isSearchUp()
+{
 	return (transmission_data[THIRD_DATA] & 0b10000000) != 0;
 }
 
-byte TEA5767N::isSearchDown() {
+byte TEA5767N::isSearchDown()
+{
 	return (transmission_data[THIRD_DATA] & 0b10000000) == 0;
 }
 
-boolean TEA5767N::isStandBy() {
+boolean TEA5767N::isStandBy()
+{
 	readStatus();
 	return (transmission_data[FOURTH_DATA] & 0b01000000) != 0;
 }
 
-void TEA5767N::setStereoReception() {
-	transmission_data[THIRD_DATA] &= 0b11110111;
+void TEA5767N::toggleForcedMono()
+{
+	forcedMono = !forcedMono;
+
+	if (forcedMono)
+	{
+		transmission_data[THIRD_DATA] |= 0b00001000;
+	}
+	else
+	{
+		transmission_data[THIRD_DATA] &= 0b11110111;
+	}
+
 	transmitData();
 }
 
-void TEA5767N::setMonoReception() {
-	transmission_data[THIRD_DATA] |= 0b00001000;
+void TEA5767N::toggleStandby()
+{
+	standby = !standby;
+
+	if (standby)
+	{
+		transmission_data[FOURTH_DATA] |= 0b01000000;
+	}
+	else
+	{
+		transmission_data[FOURTH_DATA] &= 0b10111111;
+	}
+
 	transmitData();
 }
 
-void TEA5767N::setSoftMuteOn() {
-	transmission_data[FOURTH_DATA] |= 0b00001000;
+void TEA5767N::toggleHighCutControl()
+{
+	highCutControl = !highCutControl;
+
+	if (highCutControl)
+	{
+		transmission_data[FOURTH_DATA] |= 0b00000100;
+	}
+	else
+	{
+		transmission_data[FOURTH_DATA] &= 0b11111011;
+	}
+
 	transmitData();
 }
 
-void TEA5767N::setSoftMuteOff() {
-	transmission_data[FOURTH_DATA] &= 0b11110111;
-	transmitData();
-}
+void TEA5767N::toggleStereoNoiseCancelling()
+{
+	stereoNoiseCancelling = !stereoNoiseCancelling;
 
-void TEA5767N::muteRight() {
-	transmission_data[THIRD_DATA] |= 0b00000100;
-	transmitData();
-}
+	if (stereoNoiseCancelling)
+	{
+		transmission_data[FOURTH_DATA] |= 0b00000010;
+	}
+	else
+	{
+		transmission_data[FOURTH_DATA] &= 0b11111101;
+	}
 
-void TEA5767N::turnTheRightSoundBackOn() {
-	transmission_data[THIRD_DATA] &= 0b11111011;
-	transmitData();
-}
-
-void TEA5767N::muteLeft() {
-	transmission_data[THIRD_DATA] |= 0b00000010;
-	transmitData();
-}
-
-void TEA5767N::turnTheLeftSoundBackOn() {
-	transmission_data[THIRD_DATA] &= 0b11111101;
-	transmitData();
-}
-
-void TEA5767N::setStandByOn() {
-	transmission_data[FOURTH_DATA] |= 0b01000000;
-	transmitData();
-}
-
-void TEA5767N::setStandByOff() {
-	transmission_data[FOURTH_DATA] &= 0b10111111;
-	transmitData();
-}
-
-void TEA5767N::setHighCutControlOn() {
-	transmission_data[FOURTH_DATA] |= 0b00000100;
-	transmitData();
-}
-
-void TEA5767N::setHighCutControlOff() {
-	transmission_data[FOURTH_DATA] &= 0b11111011;
-	transmitData();
-}
-
-void TEA5767N::setStereoNoiseCancellingOn() {
-	transmission_data[FOURTH_DATA] |= 0b00000010;
-	transmitData();
-}
-
-void TEA5767N::setStereoNoiseCancellingOff() {
-	transmission_data[FOURTH_DATA] &= 0b11111101;
 	transmitData();
 }
--- TEA5767N.h
+++ TEA5767N.h
@@ -16,124 +16,200 @@
 
 #include <Arduino.h>
 
-#ifndef TEA5767N_h
-#define TEA5767N_h
+#ifndef TEA5767N_H
+#define TEA5767N_H
 
 //Note that not all of these constants are being used. They are here for
 //convenience, though.
-#define TEA5767_I2C_ADDRESS           0x60
+#define TEA5767_I2C_ADDRESS 0x60
 
-#define FIRST_DATA                    0
-#define SECOND_DATA                   1
-#define THIRD_DATA                    2
-#define FOURTH_DATA                   3
-#define FIFTH_DATA                    4
+#define FIRST_DATA 0
+#define SECOND_DATA 1
+#define THIRD_DATA 2
+#define FOURTH_DATA 3
+#define FIFTH_DATA 4
 
-#define LOW_STOP_LEVEL                1
-#define MID_STOP_LEVEL                2
-#define HIGH_STOP_LEVEL               3
+#define LOW_STOP_LEVEL 1
+#define MID_STOP_LEVEL 2
+#define HIGH_STOP_LEVEL 3
 
-#define HIGH_SIDE_INJECTION           1
-#define LOW_SIDE_INJECTION            0
+#define HIGH_SIDE_INJECTION 1
+#define LOW_SIDE_INJECTION 0
 
-#define STEREO_ON                     0
-#define STEREO_OFF                    1
+#define STEREO_ON 0
+#define STEREO_OFF 1
 
-#define MUTE_RIGHT_ON                 1
-#define MUTE_RIGHT_OFF                0
-#define MUTE_LEFT_ON                  1
-#define MUTE_LEFT_OFF                 0
+#define MUTE_RIGHT_ON 1
+#define MUTE_RIGHT_OFF 0
+#define MUTE_LEFT_ON 1
+#define MUTE_LEFT_OFF 0
 
-#define SWP1_HIGH                     1
-#define SWP1_LOW                      0
-#define SWP2_HIGH                     1
-#define SWP2_LOW                      0
+#define SWP1_HIGH 1
+#define SWP1_LOW 0
+#define SWP2_HIGH 1
+#define SWP2_LOW 0
 
-#define STBY_ON                       1
-#define STBY_OFF                      0
+#define STBY_ON 1
+#define STBY_OFF 0
 
-#define JAPANESE_FM_BAND              1
-#define US_EUROPE_FM_BAND             0
+#define JAPANESE_FM_BAND 1
+#define US_EUROPE_FM_BAND 0
 
-#define SOFT_MUTE_ON                  1
-#define SOFT_MUTE_OFF                 0
+#define SOFT_MUTE_ON 1
+#define SOFT_MUTE_OFF 0
 
-#define HIGH_CUT_CONTROL_ON           1
-#define HIGH_CUT_CONTROL_OFF          0
+#define HIGH_CUT_CONTROL_ON 1
+#define HIGH_CUT_CONTROL_OFF 0
 
-#define STEREO_NOISE_CANCELLING_ON    1
-#define STEREO_NOISE_CANCELLING_OFF   0
+#define STEREO_NOISE_CANCELLING_ON 1
+#define STEREO_NOISE_CANCELLING_OFF 0
 
-#define SEARCH_INDICATOR_ON           1
-#define SEARCH_INDICATOR_OFF          0
+#define SEARCH_INDICATOR_ON 1
+#define SEARCH_INDICATOR_OFF 0
 
-class TEA5767N {
-	private:
-	  float frequency;
-	  byte hiInjection;
-	  byte frequencyH;
-	  byte frequencyL;
-	  byte transmission_data[5];
-	  byte reception_data[5];
-	  boolean muted;
-		
-	  void setFrequency(float);
-	  void transmitFrequency(float);
-	  void transmitData();
-	  void initializeTransmissionData();
-	  void readStatus();
-	  float getFrequencyInMHz(unsigned int);
-	  void calculateOptimalHiLoInjection(float);
-	  void setHighSideLOInjection();
-	  void setLowSideLOInjection();
-	  void setSoundOn();
-	  void setSoundOff();
-	  void loadFrequency();
-	  byte isReady();
-	  byte isBandLimitReached();
-		
-	public:
-	  TEA5767N();
-	  void selectFrequency(float);
-	  void selectFrequencyMuting(float);
-	  void mute();
-	  void turnTheSoundBackOn();
-	  void muteLeft();
-	  void turnTheLeftSoundBackOn();
-	  void muteRight();
-	  void turnTheRightSoundBackOn();
-	  float readFrequencyInMHz();
-	  void setSearchUp();
-	  void setSearchDown();
-	  void setSearchLowStopLevel();
-	  void setSearchMidStopLevel();
-	  void setSearchHighStopLevel();
-	  void setStereoReception();
-	  void setMonoReception();
-	  void setSoftMuteOn();
-	  void setSoftMuteOff();
-	  
-	  void setStandByOn();
-	  void setStandByOff();
-	  void setHighCutControlOn();
-	  void setHighCutControlOff();
-	  void setStereoNoiseCancellingOn();
-	  void setStereoNoiseCancellingOff();
-	  
-	  byte searchNext();
-	  byte searchNextMuting();
-	  byte startsSearchFrom(float frequency);
-	  byte startsSearchFromBeginning();
-	  byte startsSearchFromEnd();
-	  byte startsSearchMutingFromBeginning();
-	  byte startsSearchMutingFromEnd();
-	  byte getSignalLevel();
-	  byte isStereo();
-	  byte isSearchUp();
-	  byte isSearchDown();
-	  boolean isMuted();
-	  boolean isStandBy();
-	  
+class TEA5767N
+{
+private:
+  float frequency;
+  byte hiInjection;
+  byte frequencyH;
+  byte frequencyL;
+
+  /*
+		*
+		* Transmission data layout:
+		*
+		* 	FIRST_DATA:
+		* 		{bit 7}   MUTE: if MUTE = 1 then L and R audio are muted; if MUTE = 0 then L and R audio are not muted
+		* 		{bit 6}   SM (Search Mode): if SM = 1 then in search mode; if SM = 0 then not in search mode
+		* 		{bit 5:0} PLL[13:8]: setting of synthesizer programmable counter for search or preset
+		*
+		* 	SECOND_DATA:
+		* 		{bit 7:0} PLL[7:0]: setting of synthesizer programmable counter for search or preset
+		*
+		* 	THIRD_DATA:
+		* 		{bit 7}   SUD (Search Up/Down): if SUD = 1 then search up; if SUD = 0 then search down
+		* 		{bit 6:5} SSL[1:0] (Search Stop Level):
+		* 			[SSL1][SSL0] - search stop level
+		* 			00 - not allowed in search mode
+		* 			01 - low; level ADC output = 5
+		* 			10 - mid; level ADC output = 7
+		* 			11 - high; level ADC output = 10
+		* 		{bit 4}   HLSI (High/Low Side Injection): if HLSI = 1 then high side LO injection; if HLSI = 0 then low side LO injection
+		* 		{bit 3}   MS (Mono to Stereo): if MS = 1 then forced mono; if MS = 0 then stereo ON
+		* 		{bit 2}   MR (Mute Right): if MR = 1 then the right audio channel is muted and forced mono; if MR = 0 then the right audio channel is not muted
+		* 		{bit 1}   ML (Mute Left): if ML = 1 then the left audio channel is muted and forced mono; if ML = 0 then the left audio channel is not muted
+		* 		{bit 0}   SWP1 (Software programmable port 1): if SWP1 = 1 then port 1 is HIGH; if SWP1 = 0 then port 1 is LOW
+		*
+		* 	FOURTH_DATA:
+		* 		{bit 7} SWP2 (Software programmable port 2): if SWP2 = 1 then port 2 is HIGH; if SWP2 = 0 then port 2 is LOW
+		* 		{bit 6} STBY (Standby): if STBY = 1 then in Standby mode; if STBY = 0 then not in Standby mode
+		* 		{bit 5} BL (Band Limits): if BL = 1 then Japanese FM band; if BL = 0 then US/Europe FM band
+		* 		{bit 4} XTAL (Clock frequency):
+		* 			[PLLREF][XTAL] - Clock frequency
+		* 			00 - 13 MHz
+		* 			01 - 32.678 kHz
+		* 			10 - 6.5MHz
+		* 			11 - not allowed
+		* 		{bit 3} SMUTE (Soft Mute): if SMUTE = 1 then soft mute is ON; if SMUTE = 0 then soft mute is OFF
+		* 		{bit 2} HCC (High Cut Control): if HCC = 1 then high cut control is ON; if HCC = 0 then high cut control is OFF
+		* 		{bit 1} SNC (Stereo Noise Cancelling): if SNC = 1 then stereo noise cancelling is ON; if SNC = 0 then stereo noise cancelling is OFF
+		* 		{bit 0} SI (Search Indicator): if SI = 1 then pin SWPORT1 is output for the ready flag; if SI = 0 then pin SWPORT1 is software programmable port 1
+		*
+		* 	FIFTH_DATA:
+		* 		{bit 7}   PLLREF: if PLLREF = 1 then the 6.5 MHz reference frequency for the PLL is enabled; if PLLREF = 0 then the 6.5 MHz reference frequency for the PLL is disabled; see table at XTAL
+		* 		{bit 6}   DTC: if DTC = 1 then the de-emphasis time constant is 75 microseconds; if DTC = 0 then the de-emphasis time constant is 50 microseconds
+		* 		{bit 5:0} not used; position is don’t care
+		*
+		*/
+  byte transmission_data[5];
+
+  /*
+		*
+		* Reception data layout:
+		*
+		* 	FIRST_DATA:
+		* 		{bit 7}   RF (Ready Flag): if RF = 1 then a station has been found or the band limit has been reached; if RF = 0 then no station has been found
+		* 		{bit 6}   BLF (Band Limit Flag): if BLF = 1 then the band limit has been reached; if BLF = 0 then the band limit has not been reached
+		* 		{bit 5:0} PLL[13:8]: setting of synthesizer programmable counter after search or preset
+		*
+		* 	SECOND_DATA:
+		* 		{bit 7:0} PLL[7:0]: setting of synthesizer programmable counter after search or preset
+		*
+		* 	THIRD_DATA:
+		* 		{bit 7}   STEREO (Stereo indication): if STEREO = 1 then stereo reception; if STEREO = 0 then mono reception
+		* 		{bit 6:0} PLL[13:8]: IF counter result
+		*
+		* 	FOURTH_DATA:
+		* 		{bit 7:4} LEV[3:0]: level ADC output
+		* 		{bit 3:1} CI (Chip Identification): these bits have to be set to logic 0
+		* 		{bit 0}   this bit is internally set to logic 0
+		*
+		* 	FIFTH_DATA:
+		* 		{bit 7:0} reserved for future extensions; these bits are internally set to logic 0
+		*
+		*/
+  byte reception_data[5];
+
+  boolean muted;
+  boolean mutedLeft;
+  boolean mutedRight;
+  boolean softMuted;
+  boolean highCutControl;
+  boolean stereoNoiseCancelling;
+  boolean standby;
+  boolean forcedMono;
+
+  void setFrequency(float);
+  void transmitData();
+  void initializeTransmissionData();
+  void readStatus();
+  float getFrequencyInMHz(unsigned int);
+  void calculateOptimalLOInjection(float);
+  void setHighSideLOInjection();
+  void setLowSideLOInjection();
+  void loadFrequency();
+  byte isReady();
+  byte isBandLimitReached();
+
+public:
+  TEA5767N();
+  void selectFrequency(float frequency, boolean muteDuringUpdate);
+  void selectChannel(int channel, boolean muteDuringUpdate);
+  float readFrequencyInMHz();
+
+  void setSearchUp();
+  void setSearchDown();
+  void setSearchLowStopLevel();
+  void setSearchMidStopLevel();
+  void setSearchHighStopLevel();
+
+  void toggleMute();
+  void toggleMuteLeft();
+  void toggleMuteRight();
+  void toggleSoftMute();
+
+  void toggleStandby();
+  void toggleHighCutControl();
+  void toggleStereoNoiseCancelling();
+  void toggleForcedMono();
+
+  byte searchNext(boolean muteDuringSearch);
+  byte searchFrom(float frequency, boolean muteDuringSearch);
+  byte searchFromBeginning(boolean muteDuringSearch);
+  byte searchFromEnd(boolean muteDuringSearch);
+
+  byte getSignalLevel();
+
+  byte isStereo();
+  byte isSearchUp();
+  byte isSearchDown();
+
+  boolean isMuted();
+  boolean isMutedLeft();
+  boolean isMutedRight();
+  boolean isSoftMuted();
+  boolean isStandBy();
 };
 
 #endif
